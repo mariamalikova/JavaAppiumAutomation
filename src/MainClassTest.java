a3ff98768bbe1af2dd75868ed899b37dcc60ec2d
mmalikova <mmalikova@stream.ru>
2019-05-16 14:25:42 +0300

    task2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -16,4 +16,9 @@ public class MainClassTest {
 		int expected = 14;
 		Assert.assertEquals("The actual and expected values are not equal", expected, mcObject.getLocalNumber());
 	}
+
+	@Test
+	public void testGetClassNumber(){
+		Assert.assertTrue("The actual value less or equal 45",mcObject.getClassNumber()>45);
+	}
 }
