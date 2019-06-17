2224e1b06fb7c4df160ca32ee193743b6c11e6d7
mmalikova <mmalikova@stream.ru>
2019-06-17 20:41:19 +0300

    ex 5

--- src/FirstTests.java
+++ src/FirstTests.java
@@ -1,10 +1,12 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -121,6 +123,176 @@ public class FirstTests {
 		}
 	}
 
+	// Ex 5
+	@Test
+	public void saveTwotArticlesToMyList() {
+
+		String folder_name = "Learning programming";
+		String first_article_title = "Java";
+		String second_article_title = "Kotlin";
+
+
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+				"There is no search field on the screen",
+				5
+		);
+
+		waitForElementAndSendKeys(
+				By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+				first_article_title,
+				"There is no search field on the screen",
+				5
+		);
+		waitForElementPresent(
+				By.xpath("//*[@resource-id='org.wikipedia:id/fragment_search_results']"),
+				"There is no search results on the screen"
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, 'programming language')]"),
+				"There is no '"+ first_article_title +"' search results on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='org.wikipedia:id/article_menu_bookmark']"),
+				"Cannot find button to open article options",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@text='GOT IT']"),
+				"Cannot find 'GOT IT' button",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@text='Create new']"),
+				"Cannot find button to create a new reading list",
+				5
+		);
+
+		waitForElementAndSendKeys(
+				By.xpath("//*[@resource-id='org.wikipedia:id/text_input']"),
+				folder_name,
+				"Cannot put text into articles folder name input",
+				5
+		);
+		waitForElementAndClick(
+				By.xpath("//*[@text= 'OK']"),
+				"Cannot press OK button",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='org.wikipedia:id/page_toolbar_button_show_overflow_menu']"),
+				"There is no menu button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='org.wikipedia:id/page_action_overflow_feed']"),
+				"There is no feed button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='android:id/button2']"),
+				"There is no 'no thanks' button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+				"There is no search field on the screen",
+				5
+		);
+
+		waitForElementAndSendKeys(
+				By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+				second_article_title,
+				"There is no search field on the screen",
+				5
+		);
+		waitForElementPresent(
+				By.xpath("//*[@resource-id='org.wikipedia:id/fragment_search_results']"),
+				"There is no search results on the screen"
+		);
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, 'programming language')]"),
+				"There is no '"+ second_article_title +"' search results on the screen",
+				5
+		);
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='org.wikipedia:id/article_menu_bookmark']"),
+				"Cannot find button to open article options",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, '" + folder_name + "')]"),
+				"Cannot find folder " + folder_name,
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='org.wikipedia:id/page_toolbar_button_show_overflow_menu']"),
+				"There is no menu button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='org.wikipedia:id/page_action_overflow_feed']"),
+				"There is no feed button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@resource-id='android:id/button2']"),
+				"There is no 'no thanks' button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[@content-desc='My lists']"),
+				"There is no 'My lists' button on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, '" + folder_name + "')]"),
+				"There is no '" + folder_name + "' folder on the screen",
+				5
+		);
+
+		swipeElementToLeft(
+				By.xpath("//*[contains(@text, '" + second_article_title + "')]"),
+				"Cannot find '" + second_article_title + "'"
+		);
+
+		waitForElementNotPresent(
+				By.xpath("//*[contains(@text, '" + second_article_title + "')]"),
+				"There is no search field on the screen",
+				5
+		);
+
+		waitForElementAndClick(
+				By.xpath("//*[contains(@text, '" + first_article_title + "')]"),
+				"There is no search field on the screen",
+				5
+		);
+
+		WebElement title_element = waitForElementPresent(
+				By.xpath("//*[contains(@text, '" + first_article_title + "')]"),
+				"There is no search field on the screen",
+				5
+		);
+
+		Assert.assertTrue("Title does not match. Expected: " + first_article_title + ", actual: " + title_element.getText(), first_article_title.equals(title_element.getText()));
+
+	}
+
 	//------------------------------------------------------------------------------------------------------------------
 	// Search and click
 	//------------------------------------------------------------------------------------------------------------------
@@ -170,6 +342,64 @@ public class FirstTests {
 		return wait.until(
 				ExpectedConditions.presenceOfAllElementsLocatedBy(by)
 		);
+	}
+
+	//------------------------------------------------------------------------------------------------------------------
+	// Swipe
+	//------------------------------------------------------------------------------------------------------------------
+
+
+	protected void swipeUp(int timeOfSwipe){
+		TouchAction action = new TouchAction(driver);
+		Dimension size = driver.manage().window().getSize();
+		int x = size.width/2;
+		int start_y = (int) (size.height * 0.8);
+		int end_y = (int) (size.height * 0.2);
+		action
+				.press(x, start_y)
+				.waitAction(timeOfSwipe)
+				.moveTo(x, end_y)
+				.release()
+				.perform();
+	}
+
+	protected void swipeUpQuick(){
+		swipeUp(200);
+	}
+
+	protected void swipeUpToFindElement(By by, String error_msg, int max_swipes){
+
+		int swipes_cnt = 0;
+		while (driver.findElements(by).size() == 0){
+
+			if (swipes_cnt > max_swipes){
+				waitForElementPresent(by, "Cannot find element by swiping up. \n" + error_msg, 0);
+				return;
+			}
+			swipeUpQuick();
+			swipes_cnt++;
+		}
+
+	}
+
+	protected void swipeElementToLeft(By by, String error_message)
+	{
+		WebElement element = waitForElementPresent(
+				by,
+				error_message
+		);
+		int left_x = element.getLocation().getX();
+		int right_x = left_x + element.getSize().getWidth();
+		int lower_y = element.getLocation().getY();
+		int upper_y = lower_y + element.getSize().getHeight();
+		int middle_y = (upper_y + lower_y) / 2;
+		TouchAction action = new TouchAction(driver);
+		action
+				.press(right_x, middle_y)
+				.waitAction(300)
+				.moveTo(left_x, middle_y)
+				.release()
+				.perform();
 	}
 }
 
