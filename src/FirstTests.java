f9480ea10f7767db71060009e260cc776d45c2c7
mmalikova <mmalikova@stream.ru>
2019-06-19 19:53:31 +0300

    Ex 9

--- src/FirstTests.java
+++ src/FirstTests.java
@@ -131,6 +131,26 @@ public class FirstTests extends CoreTestCase {
 		Assert.assertEquals("Article title have been changed after rotation", articleSubtitleAfterRotation, articleSubtitleBeforeRotation);
 	}
 
+	// Ex 9
+	@Test
+	public void testCheckSearchResultByTitleAndSubtitle(){
 
+		SearchPageObject searchPageObject = new SearchPageObject(driver);
+
+		String expecedTitle1 = "Java";
+		String expectedSubtitle1 = "Island of Indonesia";
+		String expecedTitle2 = "JavaScript";
+		String expectedSubtitle2 = "Programming language";
+		String expecedTitle3 = "Java (programming language)";
+		String expectedSubtitle3 = "Object-oriented programming language";
+		String searchRequest = "Java";
+
+		searchPageObject.initSearchInput();
+		searchPageObject.typeSearchLine(searchRequest);
+
+		searchPageObject.waitForElementByTitleAndDescription(expecedTitle1, expectedSubtitle1);
+		searchPageObject.waitForElementByTitleAndDescription(expecedTitle2, expectedSubtitle2);
+		searchPageObject.waitForElementByTitleAndDescription(expecedTitle3, expectedSubtitle3);
+	}
 }
 
--- src/lib/UI/SearchPageObject.java
+++ src/lib/UI/SearchPageObject.java
@@ -13,7 +13,8 @@ public class SearchPageObject extends MainPageObject {
 			SEARCH_INPUT = "//*[@resource-id='org.wikipedia:id/search_src_text']",
 			SEARCH_RESULT_TITLE_ELEMENT = "//*[@resource-id='org.wikipedia:id/page_list_item_title']",
 			SEARCH_RESULT_BY_SUBSTRING_TPL = "//*[@resource-id='org.wikipedia:id/fragment_search_results']//*[contains(@text, '{SUBSTRING}')]",
-			SEARCH_CLOSE_BUTTON = "//*[@resource-id='org.wikipedia:id/search_close_btn']";
+			SEARCH_CLOSE_BUTTON = "//*[@resource-id='org.wikipedia:id/search_close_btn']",
+			SEARCH_RESULT_BY_TITLE_AND_SUBTITLE = "//*[contains(@text, '{TITLE}')]/following-sibling::*[contains(@text, '{SUBTITLE}')]/parent::*";
 
 	public SearchPageObject(AppiumDriver driver){
 		super(driver);
@@ -22,6 +23,10 @@ public class SearchPageObject extends MainPageObject {
 	/* TEMPLATES METHODS */
 	private static String getResultSearchElement(String substr) {
 		return SEARCH_RESULT_BY_SUBSTRING_TPL.replace("{SUBSTRING}", substr);
+	}
+
+	private static String getSearchResultElementByTitleAndSubtitle(String title, String subtitle){
+		return SEARCH_RESULT_BY_TITLE_AND_SUBTITLE.replace("{TITLE}", title).replace("{SUBTITLE}", subtitle);
 	}
 	/* TEMPLATES METHODS */
 
@@ -60,6 +65,11 @@ public class SearchPageObject extends MainPageObject {
 
 	public void waitForSearchResultToDisappear(){
 		this.waitForElementNotPresent(By.xpath(SEARCH_RESULT_TITLE_ELEMENT), "Search results still present on the screen", 5);
+	}
+
+	public void waitForElementByTitleAndDescription(String title, String description){
+		String searchResultXpath = getSearchResultElementByTitleAndSubtitle(title, description);
+		this.waitForElementPresent(By.xpath(searchResultXpath), "Cannot find search result by title '" + title + "' and subtitle '" + description + "'");
 	}
 
 }
